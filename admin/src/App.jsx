cc40b6a39c4cf846dc246f3c00839c04c414191a
Mirko Schaefer <codewithmirko@gmail.com>
2024-06-17 18:36:37 +0200

    Add logic, ready for test deployment

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Admin from "./pages/Admin/Admin";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Admin />
+    </div>
+  );
+}
+
+export default App;
