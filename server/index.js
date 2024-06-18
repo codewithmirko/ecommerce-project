66f24fad65817714423ea9900038bc43a0db0acb
Mirko Schaefer <codewithmirko@gmail.com>
2024-06-18 15:45:03 +0200

    Fix admin paneÃl redirect

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.post("/upload", upload.single("product"), (req, res) => {
   if (req.file && req.file.filename) {
     res.json({
       success: 1,
-      image_url: `http://localhost:${port}/images/${req.file.filename}`,
+      image_url: `https://localhost:${port}/images/${req.file.filename}`,
     });
   } else {
     res.json({
